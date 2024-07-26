f92ec60196941cd948e623030b1804e867f98043
Walter28 <walterchristian28@gmail.com>
2024-07-26 23:45:45 +0200

    RecentProject Section ✅ and SetuptSentry tool done ✅

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import Hero from "../components/Hero";
 import { FloatingNav }  from '../components/ui/FloatingNavbar'
 import Grid from "../components/Grid";
+import RecentProjects from '@/components/RecentProjects'
 import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
@@ -11,14 +13,14 @@ export default function Home() {
       <div className="max-w-7xl w-full">
 
         <FloatingNav 
-        navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]}
+        navItems={navItems}
         />
 
         <Hero />
 
         <Grid />
+
+        <RecentProjects />
 
       </div>
     </main>
