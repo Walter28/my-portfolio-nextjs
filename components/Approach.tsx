0866ca63e0beb14245f9dd9087044471db2dc31e
Walter28 <walterchristian28@gmail.com>
2024-07-28 22:29:58 +0200

    My approch done ✅

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -6,15 +6,26 @@ import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 
 export default function Approach() {
   return (
-    <>
-      <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
-        <Card title="Sheetal is Nisha" icon={<AceternityIcon />}>
+    <section className="w-full py-20">
+        <h1 className="heading">
+            My <span className="text-purple">approach</span>
+        </h1>
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
+        <Card 
+            title="Planning & Strategy" 
+            icon={<AceternityIcon order="Phase 1" />}
+            description="We'll collaborate to map out youy website's goals, target audience, and key functionalities. We'll discuss things like site structure, navigation, and content requirements."
+        >
           <CanvasRevealEffect
             animationSpeed={5.1}
-            containerClassName="bg-emerald-900"
-          />
+            containerClassName="bg-emerald-900"/>
         </Card>
-        <Card title="Nisha is Munni" icon={<AceternityIcon />}>
+
+        <Card 
+            title="Development & Progress Update" 
+            icon={<AceternityIcon order="Phase 2"/>}
+            description="Once we agree on the plan, I cue my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way."
+        >
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-black"
@@ -24,10 +35,13 @@ export default function Approach() {
             ]}
             dotSize={2}
           />
-          {/* Radial gradient for the cute fade */}
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+          
         </Card>
-        <Card title="Munni is Aditi" icon={<AceternityIcon />}>
+        <Card 
+            title="Development & Launch" 
+            icon={<AceternityIcon order="Phase 3" />}
+            description="This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up."
+        >
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-sky-600"
@@ -35,7 +49,7 @@ export default function Approach() {
           />
         </Card>
       </div>
-    </>
+    </section>
   );
 }
 
@@ -43,17 +57,19 @@ const Card = ({
   title,
   icon,
   children,
+  description,
 }: {
   title: string;
   icon: React.ReactNode;
   children?: React.ReactNode;
+  description?: string;
 }) => {
   const [hovered, setHovered] = React.useState(false);
   return (
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative h-[30rem] relative"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -73,36 +89,31 @@ const Card = ({
       </AnimatePresence>
 
       <div className="relative z-20">
-        <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
+        <div className="text-center  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
           {icon}
         </div>
-        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+        <h2 className="dark:text-white text-center text-3xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
           {title}
+        </h2>
+        <h2 
+            className="dark:text-white text-sm text-center opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200"
+            style={{color: '#e4ecff'}}
+        >
+          {description}
         </h2>
       </div>
     </div>
   );
 };
 
-const AceternityIcon = () => {
+const AceternityIcon = ({ order } : {order: string}) => {
   return (
-    <svg
-      width="66"
-      height="65"
-      viewBox="0 0 66 65"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-    >
-      <path
-        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-        stroke="currentColor"
-        strokeWidth="15"
-        strokeMiterlimit="3.86874"
-        strokeLinecap="round"
-        style={{ mixBlendMode: "darken" }}
-      />
-    </svg>
+    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+    <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+    <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+        {order}
+    </span>
+    </button>
   );
 };
 
